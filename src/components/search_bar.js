4df9611a33ff876007a4dd1ff730f5af095377af
Odd <oddweb42+gith@gmail.com>
2016-04-13 14:30:48 +0200

    post section 1 commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+// need to do this import stuff because the transpiler doesn't add it
+
+// Generates an input field.
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => console.log(event.target.value) } />
+            </div>
+        );
+                        // this.setState({ term: event.target.value })} />
+        // such html/js mix & match, very confuz.
+        // use `this.setState` when altering the state of a component/object
+        // `setState` accepts a JSON string I guess.
+    }
+
+    onInputChange(event) {
+        //console.log(event.target.value);
+    }
+}
+
+export default SearchBar;
+// "now, any file that imports `SearchBar` will get this here thing.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { yt_api_key as api_key } from '../yt_api_key';
+// imports the `React` object from ../node_modules/react
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
+// Imports the `SearchBar` component/object from ./components/search_bar
+// (`src` is the directory this file is in)
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component.
+// This component produces HTML.
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// var func = () => {...}
+// equivalent to
+// var func = function() {...}
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    ); // I guess this is the HTML inside the JSX? Or just JSX?
+}
+// Take this component's HTML and shove it into the DOM/put it on the page
+
+ReactDOM.render(<App />,
+                document.querySelector('.container'));
+// ... dang this is ugly.
+// document.querySelector('.container') => returns a DOM element with
+// class `container`
